3ca95509eaadd10e6f669905cd6f900952e19f5e
Ashish <shawashis500@gmail.com>
2025-06-03 12:00:26 +0530

    Category all done

--- Controller/categoryController.js
+++ Controller/categoryController.js
@@ -11,7 +11,7 @@ const createCategory = async (req, res) => {
         const existingCategory = await Category.findOne({ name });
 
         if (existingCategory) {
-            return res.json({ error: "Already exists" });
+            return res.status(400).json({ message: "Already exists" });
         }
 
         const newCategory = Category({ name })
@@ -23,6 +23,67 @@ const createCategory = async (req, res) => {
     }
 }
 
+const allCategories = async (req, res) => {
+    try {
+        const allData = await Category.find({});
+        return res.status(200).send({ success: true, message: "All categories fetched successfully.", data: allData });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Server error while fetching all categories list.", error })
+    }
+}
+
+const fetchSingleCategory = async (req, res) => {
+    try {
+        const { category_id } = req.params;
+        if (!category_id) {
+            return res.status(400).json({ message: "Category_id is required." })
+        }
+        const catExists = await Category.findOne({ _id: category_id });
+        if (!catExists) {
+            return res.status(404).send({ message: "Category not found" })
+        }
+        return res.status(200).send({ success: true, message: "Single Category fetched successfully.", data: catExists });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Server error while fetching a single category.", error })
+    }
+}
+
+const deleteCategory = async (req, res) => {
+    try {
+        const { category_id } = req.params;
+        const catExists = await Category.findOne({ _id: category_id });
+        if (!catExists) {
+            return res.status(404).send({ message: "Category not found" })
+        }
+        const removeData = await Category.deleteOne({ _id: category_id });
+        return res.status(200).send({ success: true, message: "Category deleted successfully." })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Server error while deleting a single category.", error })
+    }
+}
+
+const updateCategory = async (req, res) => {
+    try {
+        const { category_id } = req.params;
+        const { name } = req.body;
+        const catExists = await Category.findOne({ _id: category_id });
+        if (!catExists) {
+            return res.status(404).send({ message: "Category not found" })
+        }
+        const updateData = await Category.updateOne({ _id: category_id }, { name: name }, { new: true });
+        return res.status(200).send({ success: true, message: "Category updated succesfully.", data: updateData })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Server error while updating a single category.", error })
+    }
+}
 module.exports = {
     createCategory,
+    allCategories,
+    fetchSingleCategory,
+    deleteCategory,
+    updateCategory,
 }
--- Routes/categoryRoute.js
+++ Routes/categoryRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { createCategory } = require("../Controller/categoryController");
+const { createCategory, allCategories, fetchSingleCategory, deleteCategory, updateCategory } = require("../Controller/categoryController");
 const { userAuth, adminAuth } = require("../Middleware/Authentication");
 const router = express.Router();
 
 router.post("/add-category", userAuth, adminAuth, createCategory);
-
+router.get("/fetch-categories", userAuth, adminAuth, allCategories);
+router.get("/get-single-category/:category_id", userAuth, adminAuth, fetchSingleCategory);
+router.delete("/delete-category/:category_id", userAuth, adminAuth, deleteCategory);
+router.put("/update-category/:category_id", userAuth, adminAuth, updateCategory);
 
 module.exports = router 
